3177714c306536d49941973f2d0a7b7b15977625
lcarlosilva <luizvaldeco@gmail.com>
2021-04-17 09:51:26 -0300

    organizacao na classe

--- app/src/main/java/com/luiz/mobile/poctonegenerator/ToneAdapter.kt
+++ app/src/main/java/com/luiz/mobile/poctonegenerator/ToneAdapter.kt
@@ -11,15 +11,14 @@ class ToneAdapter(context: Context?, resource: Int, var toneArray: Array<Tone>) 
     ArrayAdapter<Tone?>(context!!, resource, toneArray) {
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        var row = convertView
+        var row: View? = convertView
         if (row == null) {
             row = LayoutInflater.from(parent.context).inflate(R.layout.row, parent, false)
         }
-        val listName = row!!.findViewById<View>(R.id.name) as TextView
-        listName.text = (String.format("%02d", toneArray[position].toneType)+ " : " + toneArray[position].toneName)
-        val listDesc = row.findViewById<View>(R.id.desc) as TextView
-        listDesc.text = toneArray[position].toneDesc
-        return row
+        val listName = row?.findViewById<TextView>(R.id.name)
+        listName?.text = (String.format("%02d", toneArray[position].toneType) + " : " + toneArray[position].toneName)
+        val listDesc = row?.findViewById<TextView>(R.id.desc)
+        listDesc?.text = toneArray[position].toneDesc
+        return row as View
     }
-
 }
